5d5236c8e9d10cf8792fdab5cbd2d14f96f47821
Shashank Neelagiri <sneelagiri@outlook.com>
2020-01-09 19:14:01 +0100

    Set up js files and declare submitComment

---
+++ academy-work/week-1/day-3/museum-app/pages/detail-page.js
@@ -0,0 +1,61 @@
+function doesNotPassAllValidations(name, msg) {
+    if (!name && !msg) {
+        alert('You forgot to fill in your name and message!')
+        return true
+    }
+
+    if (!name) {
+        alert('You forgot to fill in your name')
+        return true
+    }
+
+    if (name )
+
+    if (!msg) {
+        alert('You forgot to fill in your message')
+        return true
+    }
+
+    if (msg.length > 280) {
+        alert('Your comment is too long')
+        return true
+    }
+
+    //add more validations
+
+    return false
+}
+
+function submitComment() {
+    const inputField = document.getElementById('name')
+    const name = inputField.value
+    const inputField2 = document.getElementById('msg')
+    const msg = inputField2.value
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+    
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+
+    console.log(comment)
+    
+    const commentSection = document.getElementById('comments')
+
+    console.log(commentSection)
+
+    console.log('name test:', name)
+    console.log('msg test:', msg)
+
+    if(doesNotPassAllValidations(name, msg)) {
+        return null
+    }
+
+    inputField.value = null
+    inputField2.value = null
+
+    commentSection.appendChild(comment)
+}
