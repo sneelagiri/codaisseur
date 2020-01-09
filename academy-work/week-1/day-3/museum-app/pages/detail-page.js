710571bf71a5017b80b7beed4e32f389b0733a11
Shashank Neelagiri <sneelagiri@outlook.com>
2020-01-09 16:06:59 +0100

    Create elements with data from form

--- academy-work/week-1/day-3/museum-app/pages/detail-page.js
+++ academy-work/week-1/day-3/museum-app/pages/detail-page.js
@@ -1,3 +1,18 @@
 function submitComment() {
-    console.log("Hello!")
+    const inputField = document.getElementById('name')
+    const name = inputField.value
+    const inputField2 = document.getElementById('msg')
+    const msg = inputField2.value
+    const comment = document.getElementById('comments')
+    const h3 = document.getElementById('commenter')
+    const p = document.getElementById('theComment')
+    
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+
+    console.log(comment)
+    
 }
