66b9f7789576d71d59f85629bbc8ac1416654f7b
Shashank Neelagiri <sneelagiri@outlook.com>
2020-03-04 14:21:04 +0100

    finished implementing authentication

--- academy-work/week-9/graphql+prisma/src/index.js
+++ academy-work/week-9/graphql+prisma/src/index.js
@@ -1,74 +1,25 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
-
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    // 2
-    feed: (root, args, context, info) => {
-      return context.prisma.links();
-    }
-    // link: (parent, args) => {
-    //   // console.log("WHAT ARE THE ARGUMENTS?", args);
-    //   let match = {};
-    //   links.map(link => {
-    //     if (link.id === args.id) {
-    //       match = link;
-    //       return link;
-    //     } else {
-    //       return link;
-    //     }
-    //   });
-    //   return match;
-    // }
-  },
-  // 3
-  Mutation: {
-    // 2
-    post: (root, args, context) => {
-      return context.prisma.createLink({
-        url: args.url,
-        description: args.description
-      });
-    }
-    // updateLink: (parent, args) => {
-    //   // console.log("WHAT ARE THE ARGUMENTS?", args);
-    //   let newLink = {};
-    //   const newLinks = links.map(link => {
-    //     if (link.id === args.id) {
-    //       link.description = args.description;
-    //       link.url = args.url;
-    //       newLink = link;
-    //       return newLink;
-    //     }
-    //     return link;
-    //   });
-    //   links = newLinks;
-    //   // console.log("WHAT IS LINK?", newLink);
-    //   // console.log(newLinks);
-    //   return newLink;
-    // },
-    // deleteLink: (parent, args) => {
-    //   let deletedLink = {};
-    //   links.map(link => {
-    //     if (link.id === args.id) {
-    //       deletedLink = link;
-    //       return link;
-    //     } else {
-    //       return link;
-    //     }
-    //   });
-    //   const newLinks = links.filter(link => link.id !== args.id);
-    //   console.log(newLinks);
-    //   return deletedLink;
-    // }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 };
 
 const server = new GraphQLServer({
   typeDefs: `./src/schema.graphql`,
   resolvers,
-  context: { prisma }
+  context: request => {
+    return {
+      ...request,
+      prisma
+    };
+  }
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
--- academy-work/week-9/graphql+prisma/src/resolvers/Mutation.js
+++ academy-work/week-9/graphql+prisma/src/resolvers/Mutation.js
@@ -40,6 +40,15 @@ async function login(parent, args, context, info) {
   };
 }
 
+function post(parent, args, context, info) {
+  const userId = getUserId(context);
+  return context.prisma.createLink({
+    url: args.url,
+    description: args.description,
+    postedBy: { connect: { id: userId } }
+  });
+}
+
 module.exports = {
   signup,
   login,
